73f85e9fa2ce360cef3b1d701b18d06bfd9fa81c
cattaku <wish8436@gmail.com>
2020-12-22 00:06:41 +0900

    client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage';
+import Login from './components/views/user/Login';
+import Register from './components/views/user/Register';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" component = {LandingPage} />
+          <Route exact path="/login" component = {Login} />
+          <Route exact path="/register" component = {Register} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/components/views/LandingPage.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+import { withRouter } from 'react-router-dom'; 
+
+function LandingPage() {
+    return (
+        <div>
+            
+            <p>LandingPage</p>
+            <p>LandingPage</p>
+        </div>
+    )
+}
+
+export default withRouter(LandingPage)
